a112d05595fe302e0ee1a0dbd713e5d78429b215
LucaVitale00 <luca16vitale2000@gmail.com>
2023-01-31 12:44:51 +0100

    ricarico progetto per errore

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('pages.home');
+    return view('pages.comics');
 });
 
-Route::get('/details', function () {
-    return view('pages.details');
+Route::get('/comic', function () {
+    return view('pages.comic');
 });
